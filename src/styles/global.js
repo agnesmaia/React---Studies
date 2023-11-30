59406adfb77fb287a08c9aac33ebf5dced9e4c5d
pedro.troccoli <pedro.troccoli@appertech.com.br>
2023-11-30 20:12:19 -0300

    Initial commit

---
+++ src/styles/global.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle `
+    body {
+        background: ${props => props.theme.backgroundColor };
+        font-family: sans-serif;
+        color: ${props => props.theme.textColor};
+    }
+`;
